57f4c82143398c23b26b131816d5be2835ca3bdf
YS.VostroPC <rotace2012227ys@gmail.com>
2017-07-17 00:48:23 +0900

    reformat code

--- python/main.py
+++ python/main.py
@@ -25,6 +25,19 @@ print("## sip    ",SIP_VERSION_STR)
 
 from gui import main_window
 
+
+size_list = []
+size_head =      ['Name' , 'w', 'h']
+size_list.append(['QQVGA', 160, 120])
+size_list.append(['QVGA' , 320, 240])
+size_list.append(['VGA'  , 640, 480])
+
+mode_list = []
+mode_head =      ['Name'      ,'d', 'QtFormat']
+mode_list.append(['Grayscale8', 1,  QtGui.QImage.Format_Grayscale8])
+mode_list.append(['RGB444'    , 2,  QtGui.QImage.Format_RGB444])
+
+
 class MainForm(QtWidgets.QMainWindow, main_window.Ui_MainWindow):
     def __init__(self):
         super(self.__class__, self).__init__()
@@ -52,36 +65,39 @@ class MainForm(QtWidgets.QMainWindow, main_window.Ui_MainWindow):
         print('update_image')
         print(buf[0:1],buf[-8:])
 
-        h=240
-        w=320
-        d=1   # 2*uint8 :2byte:16bitRGB
+        size_dict = dict(zip(size_head, size_list[1]))
+        mode_dict = dict(zip(mode_head, mode_list[0]))
+        w=size_dict['w']
+        h=size_dict['h']
+        d=mode_dict['d']
+        qt_format = mode_dict['QtFormat']
 
-        if buf[0:1]!=b'\xff': return
-        if buf[-8:]!=b'\x00\x00\x00\x00\x00\x00\x00\x00': return
+        if buf[0:1]!=b'\xff': return # header
+        if buf[-8:]!=b'\x00\x00\x00\x00\x00\x00\x00\x00': return # footer
         print(len(buf[1:-8]))
         # assert(len(buf[1:-8])==h*w)
         # self.image = np.fromstring(buf[1:-8], dtype=np.uint8)
-        self.qimage = QtGui.QImage(buf[1:-8], w, h, w*d, QtGui.QImage.Format_Grayscale8)
+        self.qimage = QtGui.QImage(buf[1:-8], w, h, w*d, qt_format)
         self.pixmap = QtGui.QPixmap.fromImage(self.qimage)
         self.pixitem.setPixmap(self.pixmap)
 
-    def test(self):
-        h=240
-        w=320
-        d=2   # 2*uint8 :2byte:16bitRGB
-        image=np.zeros((h,w,d), dtype='uint8')
-
-        for i in range(h):
-            for j in range(w):
-                r = int(i*0b1111/h)
-                g = int(j*0b1111/w)
-                b = int(0)
-                image[i,j,0] = g << 4 | b
-                image[i,j,1] = 0 << 4 | r
-
-        self.qimage = QtGui.QImage(image.data, w, h, w*d, QtGui.QImage.Format_RGB444)
-        self.pixmap = QtGui.QPixmap.fromImage(self.qimage)
-        self.pixitem.setPixmap(self.pixmap)
+    # def test(self):
+    #     h=240
+    #     w=320
+    #     d=2   # 2*uint8 :2byte:16bitRGB
+    #     image=np.zeros((h,w,d), dtype='uint8')
+    #
+    #     for i in range(h):
+    #         for j in range(w):
+    #             r = int(i*0b1111/h)
+    #             g = int(j*0b1111/w)
+    #             b = int(0)
+    #             image[i,j,0] = g << 4 | b
+    #             image[i,j,1] = 0 << 4 | r
+    #
+    #     self.qimage = QtGui.QImage(image.data, w, h, w*d, QtGui.QImage.Format_RGB444)
+    #     self.pixmap = QtGui.QPixmap.fromImage(self.qimage)
+    #     self.pixitem.setPixmap(self.pixmap)
 
 
 
@@ -105,25 +121,6 @@ class Reciever(QtCore.QThread):
         usbserial_names = serial.tools.list_ports.comports()
         usbserial_names = list(filter(lambda x: x[2]!='n/a', usbserial_names ))
         self.usbserial = serial.Serial(usbserial_names[0][0], 1000000)
-
-        # h=240
-        # w=320
-        # d=2   # 2*uint8 :2byte:16bitRGB
-        # self.image = np.zeros((h,w,d), dtype='uint8')
-        #
-        # for i in range(h):
-        #     for j in range(w):
-        #         r = int(i*0b1111/h)
-        #         g = int(j*0b1111/w)
-        #         b = int(0)
-        #         self.image[i,j,0] = g << 4 | b
-        #         self.image[i,j,1] = 0 << 4 | r
-        #
-        # self.mutex.lock()
-        # self.buf_list.append(b''.join([b'\xff',self.image.tobytes(),b'\x00\x00\x00\x00\x00\x00\x00\x00']))
-        # print(self.buf_list[-1], len(self.buf_list[-1]))
-        # self.mutex.unlock()
-        # self.signalObject.emit()
 
         buf=[]
         while True:
