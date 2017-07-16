3a0e3feecf5cfdd05e7c75b9e5862277bdcfd158
YS.VostroPC <rotace2012227ys@gmail.com>
2017-07-17 00:02:03 +0900

    implemented grayscale mode

---
+++ python/gui/main_window.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'main_window.ui'
+#
+# Created by: PyQt5 UI code generator 5.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(666, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.widget_1 = QtWidgets.QWidget(self.centralwidget)
+        self.widget_1.setObjectName("widget_1")
+        self.verticalLayout_1 = QtWidgets.QVBoxLayout(self.widget_1)
+        self.verticalLayout_1.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_1.setObjectName("verticalLayout_1")
+        self.graphicsView = QtWidgets.QGraphicsView(self.widget_1)
+        self.graphicsView.setObjectName("graphicsView")
+        self.verticalLayout_1.addWidget(self.graphicsView)
+        self.horizontalLayout.addWidget(self.widget_1)
+        self.widget_2 = QtWidgets.QWidget(self.centralwidget)
+        self.widget_2.setObjectName("widget_2")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.widget_2)
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.groupBox = QtWidgets.QGroupBox(self.widget_2)
+        self.groupBox.setObjectName("groupBox")
+        self.verticalLayout_2.addWidget(self.groupBox)
+        self.horizontalLayout.addWidget(self.widget_2)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 666, 25))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.groupBox.setTitle(_translate("MainWindow", "option"))
+
---
+++ python/main.py
@@ -0,0 +1,150 @@
+
+import sys
+import os
+import binascii
+import time
+
+import serial
+import serial.tools.list_ports
+from PyQt5 import QtGui, QtWidgets, QtCore
+import numpy as np
+
+# version check
+from platform import python_version
+from PyQt5.QtCore import QT_VERSION_STR
+from PyQt5.Qt import PYQT_VERSION_STR
+from sip import SIP_VERSION_STR
+print("## python ",python_version())
+print("## Qt     ",QT_VERSION_STR)
+print("## PyQt   ",PYQT_VERSION_STR)
+print("## sip    ",SIP_VERSION_STR)
+# assert(python_version() == '3.5.2')
+# assert(QT_VERSION_STR == '5.6.2')
+# assert(PYQT_VERSION_STR == '5.6')
+# assert(SIP_VERSION_STR == '4.18')
+
+from gui import main_window
+
+class MainForm(QtWidgets.QMainWindow, main_window.Ui_MainWindow):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.setupUi(self)
+
+        self.show()
+
+        self.scene = QtWidgets.QGraphicsScene()
+        self.pixitem = QtWidgets.QGraphicsPixmapItem()
+        self.scene.addItem(self.pixitem)
+        self.graphicsView.setScene(self.scene)
+
+        self.reciever = Reciever()
+        self.reciever.signalObject.connect(self.update_image)
+
+        self.buf_list = []
+
+        #
+        # self.test()
+        self.reciever.setup(self.buf_list)
+        self.reciever.start()
+
+    def update_image(self):
+        buf=self.buf_list.pop(0)
+        print('update_image')
+        print(buf[0:1],buf[-8:])
+
+        h=240
+        w=320
+        d=1   # 2*uint8 :2byte:16bitRGB
+
+        if buf[0:1]!=b'\xff': return
+        if buf[-8:]!=b'\x00\x00\x00\x00\x00\x00\x00\x00': return
+        print(len(buf[1:-8]))
+        # assert(len(buf[1:-8])==h*w)
+        # self.image = np.fromstring(buf[1:-8], dtype=np.uint8)
+        self.qimage = QtGui.QImage(buf[1:-8], w, h, w*d, QtGui.QImage.Format_Grayscale8)
+        self.pixmap = QtGui.QPixmap.fromImage(self.qimage)
+        self.pixitem.setPixmap(self.pixmap)
+
+    def test(self):
+        h=240
+        w=320
+        d=2   # 2*uint8 :2byte:16bitRGB
+        image=np.zeros((h,w,d), dtype='uint8')
+
+        for i in range(h):
+            for j in range(w):
+                r = int(i*0b1111/h)
+                g = int(j*0b1111/w)
+                b = int(0)
+                image[i,j,0] = g << 4 | b
+                image[i,j,1] = 0 << 4 | r
+
+        self.qimage = QtGui.QImage(image.data, w, h, w*d, QtGui.QImage.Format_RGB444)
+        self.pixmap = QtGui.QPixmap.fromImage(self.qimage)
+        self.pixitem.setPixmap(self.pixmap)
+
+
+
+class Reciever(QtCore.QThread):
+    signalObject = QtCore.pyqtSignal()
+
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.stopped = False
+        self.mutex = QtCore.QMutex()
+
+    def setup(self, buf_list):
+        self.buf_list = buf_list
+        self.stopped = False
+
+    def stop(self):
+        with QtCore.QMutexLocker(self.mutex):
+            self.stopped = True
+
+    def run(self):
+        usbserial_names = serial.tools.list_ports.comports()
+        usbserial_names = list(filter(lambda x: x[2]!='n/a', usbserial_names ))
+        self.usbserial = serial.Serial(usbserial_names[0][0], 1000000)
+
+        # h=240
+        # w=320
+        # d=2   # 2*uint8 :2byte:16bitRGB
+        # self.image = np.zeros((h,w,d), dtype='uint8')
+        #
+        # for i in range(h):
+        #     for j in range(w):
+        #         r = int(i*0b1111/h)
+        #         g = int(j*0b1111/w)
+        #         b = int(0)
+        #         self.image[i,j,0] = g << 4 | b
+        #         self.image[i,j,1] = 0 << 4 | r
+        #
+        # self.mutex.lock()
+        # self.buf_list.append(b''.join([b'\xff',self.image.tobytes(),b'\x00\x00\x00\x00\x00\x00\x00\x00']))
+        # print(self.buf_list[-1], len(self.buf_list[-1]))
+        # self.mutex.unlock()
+        # self.signalObject.emit()
+
+        buf=[]
+        while True:
+            tmp = self.usbserial.read(self.usbserial.inWaiting())
+            if len(tmp)!=0:
+                buf.append(tmp)
+            if tmp.endswith(b'\x00\x00\x00\x00\x00\x00\x00\x00'):
+                self.mutex.lock()
+                self.buf_list.append(b''.join(buf))
+                buf=[]
+                # print(self.buf_list[-1], len(self.buf_list[-1]))
+                self.mutex.unlock()
+                self.signalObject.emit()
+
+        self.stop()
+        self.finished.emit()
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    form = MainForm()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
